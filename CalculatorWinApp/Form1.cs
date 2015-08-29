842ee6a8c1f06b8972d5eb222bf4a4d71f4bc882
Tanzil <tanjiluits70@gmail.com>
2015-08-28 21:56:43 -0700

    calculator app added

---
+++ CalculatorWinApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculatorWinApp
+{
+    public partial class CalculatorUI : Form
+    {
+        private double firstNumber;
+        private double secondNumber;
+
+        public CalculatorUI()
+        {
+            InitializeComponent();
+        }
+        
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstTextBox.Text);
+            secondNumber = Convert.ToDouble(secondTextBox.Text);
+            resultTextBox.Text = Convert.ToString(firstNumber + secondNumber);
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstTextBox.Text);
+            secondNumber = Convert.ToDouble(secondTextBox.Text);
+            resultTextBox.Text = Convert.ToString(firstNumber - secondNumber);
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstTextBox.Text);
+            secondNumber = Convert.ToDouble(secondTextBox.Text);
+            resultTextBox.Text = Convert.ToString(firstNumber * secondNumber);
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstTextBox.Text);
+            secondNumber = Convert.ToDouble(secondTextBox.Text);
+            resultTextBox.Text = Convert.ToString(firstNumber / secondNumber);
+        }
+    }
+}
